e11eb3e479c98e7dadcee46fe24f2f4502297ddb
javatodev <sendtospringboot@gmail.com>
2022-02-17 00:42:26 +0530

    file conversion util done

--- src/handlers/file-conversion-lambda.js
+++ src/handlers/file-conversion-lambda.js
@@ -1,10 +1,49 @@
-exports.processFileUpload = async (event) => {
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const gm = require("gm").subClass({ imageMagick: true });
+
+exports.processFileUpload = async (event, context) => {
   console.log(`Incoming Request ${JSON.stringify(event)}`);
 
-  const message = "Hello from Lambda!";
+  s3MetaData = event.Records[0].s3;
+  bucketName = s3MetaData.bucket.name;
+  fileKey = s3MetaData.object.key;
 
-  // All log statements are written to CloudWatch
-  console.info(`${message}`);
+  console.log(
+    `S3 Metadata bucketname : ${bucketName} and uploaded file key : ${fileKey}`
+  );
 
-  return message;
+  id = context.awsRequestId;
+
+  extension = fileKey.split(".")[1];
+
+  tempPath = path.join(os.tmpdir(), id + "." + extension);
+  outputPath = path.join(os.tmpdir(), "converted-" + id + "." + extension);
+
+  console.log(
+    `Temporary file path created ${tempPath} with file extension ${extension}`
+  );
+
+  return s3
+    .getObject({ Bucket: bucketName, Key: fileKey })
+    .promise()
+    .then((data) => {
+      console.log(`File reading done using given key and bucket name`);
+
+      fs.writeFileSync(tempPath, data.Body);
+
+      returngm(tempPath).identify(function (err, value) {
+        console.log("Image reading values" + value);
+
+        if (err) {
+          console.log("Error in reading image idenficaiton" + err);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(`Error in reading S3 file ${err}`);
+    });
 };
